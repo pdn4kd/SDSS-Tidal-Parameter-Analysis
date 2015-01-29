f0ebfc7745e4f949f175d19a34eae38045afd1ad
pdn4kd <pdn4kd@mail.umkc.edu>
2015-01-29 15:42:40 -0600

    Semi-automated script expansion

--- exec0_S82.sh
+++ exec0_S82.sh
@@ -1,5 +1,4 @@
-perl SDSS_SEX_S82.pl
-chmod 755 aper_all_S82.sh
-./aper_all_S82.sh
-perl APER_To_CSV.pl
-perl SDSS_POSTAGE_STAMPS_S82.pl
+perl SDSS_Rename_S82.pl
+wget -i sdss-wget.lis
+chmod 755 Rename_PSF_S82.sh
+./Rename_PSF_S82.sh
