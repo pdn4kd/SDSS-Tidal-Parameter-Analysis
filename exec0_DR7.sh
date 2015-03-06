bd68609c91da594377f27b417d688524fef200a8
pdn4kd <pdn4kd@mail.umkc.edu>
2015-03-06 17:49:28 -0600

    DR7 automation

---
+++ exec0_DR7.sh
@@ -0,0 +1,5 @@
+perl SDSS_Rename_DR7.pl
+wget -i sdss-PSF_DR7.lis
+wget -i sdss-wget-Calibration_DR7.lis
+chmod 755 Rename_PSF_DR7.sh
+./Rename_PSF_DR7.sh
---
+++ exec1_DR7.sh
@@ -0,0 +1,5 @@
+perl SDSS_SEX_DR7.pl
+chmod 755 aper_all_DR7.sh
+./aper_all_DR7.sh
+perl APER_To_CSV.pl
+perl SDSS_POSTAGE_STAMPS_DR7.pl
