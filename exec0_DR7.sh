873c6c74d5a380ffec622441c3ab24a93813248b
Patrick N <pdn4kd@noreply.github.com>
2015-04-13 21:15:53 -0500

    rm unneded calibration download

--- exec0_DR7.sh
+++ exec0_DR7.sh
@@ -1,5 +1,4 @@
 perl SDSS_Rename_DR7.pl
 wget -i sdss-wget-PSF_DR7.lis
-wget -i sdss-wget-Calibration_DR7.lis
 chmod 755 Rename_PSF_DR7.sh
 ./Rename_PSF_DR7.sh
--- exec0_S82.sh
+++ exec0_S82.sh
@@ -1,5 +1,4 @@
 perl SDSS_Rename_S82.pl
 wget -i sdss-wget-PSF_S82.lis
-wget -i sdss-wget-Calibration_S82.lis
 chmod 755 Rename_PSF_S82.sh
 ./Rename_PSF_S82.sh
